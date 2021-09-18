f09b807fb21a69162c7c41d7742610ef10214e15
unknown <sanjeevsingh.77d@gmail.com>
2021-09-19 00:51:51 +0530

    implimented bootstrap modal

---
+++ app.js
@@ -0,0 +1,12 @@
+function button () {
+    let first_Name = document.getElementById("fname").value
+    let last_Name = document.getElementById("lname").value
+    let Email = document.getElementById("email").value
+    let Address = document.getElementById("address").value
+    let pin = document.getElementById("pin").value
+    console.log(first_Name)
+    console.log(last_Name)
+    console.log(Email)
+    console.log(Address)
+    console.log(pin)   
+}
